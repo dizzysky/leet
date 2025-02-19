3442d3edc601814edf61f3caed7544301b03ee5b
Sanjid Dewan <dsanjid@gmail.com>
2025-02-19 10:57:52 -0500

    add two numbers

---
+++ problems/02_add_two_numbers.js
@@ -0,0 +1,39 @@
+  function ListNode(val, next) {
+      this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+  }
+ 
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+	let head = new ListNode();
+	let current = head; 
+	let carry = 0;
+
+	while (l1 || l2 || carry) {
+		let sum = carry;
+
+		if (l1) {
+			sum += l1.val;
+			l1 = l1.next;
+		}
+
+		if (l2) {
+			sum += l2.val;
+			l2 = l2.next;
+		}
+
+		carry = Math.floor(sum / 10);
+		current.next = new ListNode(sum % 10);
+		current = current.next;
+	}
+
+	return head.next;
+	
+};
+
+
+console.log(addTwoNumbers([2,4,3],[5,6,4]));
