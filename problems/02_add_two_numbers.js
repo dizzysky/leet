f975a4201dbbe5a2666b9bac9ca6c2d99e0afa96
Sanjid Dewan <dsanjid@gmail.com>
2025-02-19 13:03:16 -0500

    longest substring without repetition

--- problems/02_add_two_numbers.js
+++ problems/02_add_two_numbers.js
@@ -10,25 +10,27 @@
  */
 var addTwoNumbers = function(l1, l2) {
 	let head = new ListNode();
-	let current = head; 
+	let current = head.next;
 	let carry = 0;
 
-	while (l1 || l2 || carry) {
-		let sum = carry;
 
-		if (l1) {
-			sum += l1.val;
+	while (l1 || l2 || carry) {
+		sum += carry;
+
+		if(l1) {
+			sum += l1;
 			l1 = l1.next;
 		}
 
-		if (l2) {
-			sum += l2.val;
+		if(l2) {
+			sum += l2;
 			l2 = l2.next;
 		}
 
 		carry = Math.floor(sum / 10);
 		current.next = new ListNode(sum % 10);
 		current = current.next;
+
 	}
 
 	return head.next;
